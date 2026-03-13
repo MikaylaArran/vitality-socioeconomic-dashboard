c73c7be7fa495ba95848347224b90829a26a985d
“MikaylaArran” <mikaylaarran16@gmail.com>
2026-03-13 08:55:14 +0200

    first commit

--- AV dashboard v2/vite.config.js
+++ AV dashboard v2/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH ||"/AV-Dasboard-v2"
+  base: process.env.VITE_BASE_PATH ||"/AV-dashboard-v2"
 })
